1e3ce9e9ee58403a0ef56dfdc92762a79f92ed8d
Kazuya Watanabe <kazuya.watanabe@gmail.com>
2020-01-13 15:47:36 +0900

    first commit

---
+++ .vim/coc-install.vim
@@ -0,0 +1,36 @@
+if exists(':CocInstall')
+    function! s:coc_install(extension)
+        echomsg 'Installing ' . a:extension . '. Please wait a moment.'
+        silent execute('CocInstall -sync ' . a:extension)
+    endfunction
+
+    call s:coc_install('coc-css')
+    call s:coc_install('coc-eslint')
+    call s:coc_install('coc-highlight')
+    call s:coc_install('coc-html')
+    call s:coc_install('coc-snippets')
+    call s:coc_install('coc-sql')
+    call s:coc_install('coc-tsserver')
+    call s:coc_install('coc-vimlsp')
+    call s:coc_install('coc-xml')
+    call s:coc_install('coc-yaml')
+    if executable('clangd')
+        call s:coc_install('coc-clangd')
+    endif
+    if executable('cmake')
+        call s:coc_install('coc-cmake')
+    endif
+    if executable('java')
+        call s:coc_install('coc-java')
+    endif
+    call s:coc_install('coc-json')
+    if executable('php')
+        call s:coc_install('coc-phpls')
+    endif
+    if executable('python')
+        call s:coc_install('coc-python')
+    endif
+    if ! has('win32') && executable('bash')
+        call s:coc_install('coc-sh')
+    endif
+endif
---
+++ windows/install.cmd
@@ -0,0 +1,40 @@
+@ECHO OFF
+SETLOCAL
+
+SET TARGETDIR=%~dp0..\
+
+PUSHD "%USERPROFILE%"
+
+MKDIR   .pip
+MKLINK  .pip\pip.conf               "%TARGETDIR%.pip\pip.conf"
+
+MKDIR   .ssh
+MKLINK  .ssh\aws-keypair-1.pem      "%TARGETDIR%.ssh\aws-keypair-1.pem"
+MKLINK  .ssh\config                 "%TARGETDIR%.ssh\config"
+MKLINK  .ssh\github-keypair-1.pem   "%TARGETDIR%.ssh\github-keypair-1.pem"
+
+MKDIR   vimfiles
+MKLINK  vimfiles\coc-insall.vim     "%TARGETDIR%.vim\coc-install.vim"
+MKLINK  vimfiles\coc-settings.json  "%TARGETDIR%.vim\coc-settings.json"
+MKLINK  vimfiles\gvimrc             "%TARGETDIR%.vim\gvimrc"
+MKLINK  vimfiles\vimrc              "%TARGETDIR%.vim\vimrc"
+MKLINK  /D  vimfiles  .vim
+
+MKLINK  .bash_profile               "%TARGETDIR%.bash_profile"
+MKLINK  .bashrc                     "%TARGETDIR%.bashrc"
+MKLINK  .ctags                      "%TARGETDIR%.ctags"
+MKLINK  .curlrc                     "%TARGETDIR%.curlrc"
+MKLINK  .gitconfig                  "%TARGETDIR%.gitconfig"
+MKLINK  .gitignore.global           "%TARGETDIR%.gitignore.global"
+MKLINK  .inputrc                    "%TARGETDIR%.inputrc"
+MKLINK  .profile                    "%TARGETDIR%.profile"
+MKLINK  .tigrc                      "%TARGETDIR%.tigrc"
+MKLINK  .wgetrc                     "%TARGETDIR%.wgetrc"
+
+POPD
+
+SETX MSYS "winsymlinks:nativestrict"
+
+regedit %TARGETDIR%windows\caps2ctrl.reg
+regedit %TARGETDIR%windows\guestauth.reg
+regedit %TARGETDIR%windows\longpath.reg
