663a486916da2ccae12d0763778042ab65ed18ed
Kazuya Watanabe <kazuya.watanabe@workram.com>
2021-04-20 08:46:54 +0900

    vimとかneovimとかー(・o・)

--- install.ps1
+++ install.ps1
@@ -20,19 +20,25 @@ scoop install imagemagick
 scoop install inkscape
 scoop install llvm
 scoop install make
+scoop install neovim
 scoop install nodejs
 scoop install openjdk
 scoop install php
 scoop install python
 scoop install ripgrep
 scoop install unzip
-scoop install vim
+scoop install vim-nightly
 scoop install zip
 
+$env:PYTHONHOME = scoop prefix python
+[System.Environment]::SetEnvironmentVariable('PYTHONHOME', $env:PYTHONHOME, 'User')
+
 python3 -m pip install autopep8
+python3 -m pip install neovim
 python3 -m pip install pylint
 
 npm install -g eslint
+npm install -g neovim
 npm install -g yarn
 
 New-Item -ItemType HardLink -Path $env:USERPROFILE\.gitconfig -Value $ScriptDir\.gitconfig
@@ -47,3 +53,6 @@ New-Item -ItemType HardLink -Path $env:USERPROFILE\vimfiles\vimrc -Value $Script
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -OutFile $env:USERPROFILE\vimfiles\autoload\plug.vim
 vim -S $ScriptDir\.vim\install-vimplugins.vim
 vim -S $ScriptDir\.vim\install-cocextensions.vim
+
+New-Item -ItemType Junction -Path $env:LOCALAPPDATA\nvim -Value $env:USERPROFILE\vimfiles
+New-Item -ItemType HardLink -Path $env:USERPROFILE\vimfiles\init.vim -Value $env:USERPROFILE\vimfiles\vimrc
